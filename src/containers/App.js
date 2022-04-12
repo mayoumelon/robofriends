44c6d3e8973587575fc4be85e82c0b84a15943d3
mayoumelon <mayoumelon@gmail.com>
2022-04-13 06:44:00 +0900

    Add counter

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,26 +6,17 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 function App()  {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: ''
-  //   }
-  // }
 const [robots, setRobots] = useState([]);
 const [searchfield, setSearchfield] = useState('');
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response => response.json())
-  //     .then(users => this.setState( { robots: users } ));
-  // }
+const [count, setCount] = useState(0);
+
 useEffect(() => {
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(users => setRobots(users));
   console.log(robots, searchfield);
-}, []);
+  console.log(count);
+}, [count]);
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
@@ -40,6 +31,7 @@ useEffect(() => {
   (
     <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
+      <button onClick={ () => setCount(count+1)}>Click Me!</button>
       <SearchBox searchChange={onSearchChange} />
       <Scroll>
         <ErrorBoundary>
